5ca642d23cb251c7f854e6aa92348e0c808fc0a6
bagusbpg <bagusbpg@gmail.com>
2023-10-14 20:48:57 +0700

    variable

--- hello_world/bin/hello_world.dart
+++ hello_world/bin/hello_world.dart
@@ -1,5 +1,7 @@
 import 'package:hello_world/hello_world.dart' as hello_world;
 
 void main(List<String> arguments) {
+  var greetings = 'Hello Dart!';
+  print(greetings);
   print('Hello world: ${hello_world.calculate()}!');
 }
