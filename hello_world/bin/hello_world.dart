64d3ff58720526d25d5f843608e844e748d3b027
bagusbpg <bagusbpg@gmail.com>
2023-10-14 20:52:50 +0700

    nullable

--- hello_world/bin/hello_world.dart
+++ hello_world/bin/hello_world.dart
@@ -3,5 +3,9 @@ import 'package:hello_world/hello_world.dart' as hello_world;
 void main(List<String> arguments) {
   var greetings = 'Hello Dart!';
   print(greetings);
+  var myLuckyNumber; // should be int myLuckyNumber;
+  print(myLuckyNumber); // should be null
+  myLuckyNumber = 7;
+  print(myLuckyNumber);
   print('Hello world: ${hello_world.calculate()}!');
 }
