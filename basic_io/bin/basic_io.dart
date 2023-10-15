e2b68c77e90c96d5791024305f89450c9622de33
bagusbpg <bagusbpg@gmail.com>
2023-10-15 09:52:41 +0700

    parsing and type conversion

--- basic_io/bin/basic_io.dart
+++ basic_io/bin/basic_io.dart
@@ -7,4 +7,16 @@ void main(List<String> arguments) {
   // this one will throw error if input cannot be parsed as integer
   int number = int.parse(stdin.readLineSync()!);
   print('Hello $name, $number is your lucky number');
+
+  int eleven = int.parse('11');
+  print('$eleven has type ${eleven.runtimeType}');
+
+  double elevenPointTwo = double.parse('11.2');
+  print('$elevenPointTwo has type ${elevenPointTwo.runtimeType}');
+  
+  String elevenAsString = 11.toString();
+  print('$elevenAsString has type ${elevenAsString.runtimeType}');
+  
+  String piAsString = 3.14.toStringAsFixed(2);
+  print('$piAsString has type ${piAsString.runtimeType}');
 }
