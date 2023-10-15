b496ccefa472d4666326a87ef319cc7bea8650c2
bagusbpg <bagusbpg@gmail.com>
2023-10-15 11:03:34 +0700

    arithmetic operation

---
+++ operations/bin/operations.dart
@@ -0,0 +1,8 @@
+void main(List<String> arguments) {
+  print('5 + 2 = ${5 + 2}'); // int add = 7
+  print('5 - 2 = ${5 - 2}'); // int subtract = 3
+  print('5 * 2 = ${5 * 2}'); // int multiply = 10
+  print('5 / 2 = ${5 / 2}'); // double divide = 2.5
+  print('5 ~/ 2 = ${5 ~/ 2}'); // int intDivide = 2
+  print('5 % 2 = ${5 % 2}'); // int modulo = 1
+}
