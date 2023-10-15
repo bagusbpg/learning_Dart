bf82862ac978381969eb0b63c864c07cf321e892
bagusbpg <bagusbpg@gmail.com>
2023-10-15 09:42:55 +0700

    basic input output

--- hello_world/bin/hello_world.dart
+++ hello_world/bin/hello_world.dart
@@ -3,7 +3,7 @@ import 'package:hello_world/hello_world.dart' as hello_world;
 void main(List<String> arguments) {
   var greetings = 'Hello Dart!';
   print(greetings);
-  var myLuckyNumber; // should be int myLuckyNumber;
+  var myLuckyNumber; // should be int myLuckyNumber or other explicit type;
   print(myLuckyNumber); // should be null
   myLuckyNumber = 7;
   print(myLuckyNumber);
